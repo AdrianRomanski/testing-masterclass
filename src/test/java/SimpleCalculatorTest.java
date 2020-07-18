d17ddff590a159fb0b339d7bb223b0709f2396ae
adrianromanski <adrianromanski@icloud.com>
2020-07-18 13:38:38 +0200

    Initial commit

---
+++ src/test/java/SimpleCalculatorTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleCalculatorTest {
+
+    SimpleCalculator calculator;
+
+    @Test
+    void shouldReturnANumberWhenNumberGiven() {
+        getCalculator();
+        assertEquals(2, calculator.add("2"));
+    }
+
+    @Test
+    void shouldReturnSumTwoNumbersWhenTwoNumbersGiven() {
+        getCalculator();
+        assertEquals(3, calculator.add("2,1"));
+    }
+
+    private void getCalculator() {
+        calculator = new SimpleCalculator();
+    }
+
+}
